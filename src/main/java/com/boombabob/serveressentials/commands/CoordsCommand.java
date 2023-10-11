26baa271de47962165bbeed5860d39f5ddcb9275
james <james@mazuma.net.au>
2023-10-11 19:48:22 +1300

    Edit to work more similarly to message command

--- src/main/java/com/boombabob/serveressentials/commands/CoordsCommand.java
+++ src/main/java/com/boombabob/serveressentials/commands/CoordsCommand.java
@@ -3,9 +3,11 @@ package com.boombabob.serveressentials.commands;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.tree.LiteralCommandNode;
+import net.minecraft.network.message.MessageType;
+import net.minecraft.network.message.SentMessage;
+import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
 
 import java.util.Collection;
 
@@ -37,20 +39,25 @@ public class CoordsCommand {
     }
 
     public static int sendCoords(ServerPlayerEntity sender, Collection<ServerPlayerEntity> recipients) {
-        String senderName = sender.getName().toString();
-        senderName = senderName.substring(8, senderName.length() - 1);
         String senderDimension = sender.getServerWorld().getRegistryKey().getValue().toString();
         senderDimension = senderDimension.substring(senderDimension.indexOf(":") + 1);
         int senderX = (int) sender.getX();
         int senderY = (int) sender.getY();
         int senderZ = (int) sender.getZ();
-        String sendingPlayerPos = "%s is at %d, %d, %d in the %s"
-            .formatted(senderName, senderX, senderY, senderZ, senderDimension);
+        String sendingPlayerPos = "I am at %d, %d, %d in the %s"
+            .formatted(senderX, senderY, senderZ, senderDimension);
         for (ServerPlayerEntity recipient : recipients) {
-            recipient.sendMessage(Text.literal(sendingPlayerPos), false);
+            recipient.sendChatMessage(SentMessage.of(
+                SignedMessage.ofUnsigned(sendingPlayerPos)),
+                false,
+                MessageType.params(MessageType.MSG_COMMAND_INCOMING, sender.getCommandSource())
+            );
+            sender.sendChatMessage(SentMessage.of(
+                SignedMessage.ofUnsigned(sendingPlayerPos)),
+                false,
+                MessageType.params(MessageType.MSG_COMMAND_OUTGOING, recipient.getCommandSource())
+            );
         }
-        sender.sendMessage(Text.literal("You are at %d, %d, %d in the %s"
-            .formatted(senderX, senderY, senderZ, senderDimension)));
         return Command.SINGLE_SUCCESS;
     }
 }
